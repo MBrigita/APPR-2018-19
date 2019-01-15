3cc42c57f84c25db434d0d6670ff009442c2a153
Brigita Milanic <brigita.m97@gmail.com>
2019-01-15 20:12:34 +0100

    dodan še en graf, legende še vedno ne delaj0

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -20,35 +20,39 @@ zemljevid <- filter(zemljevid, CONTINENT == "Europe" | SOVEREIGNT %in% c("Turkey
 ggplot() + geom_polygon(data=zemljevid, aes(x=long, y=lat, group=group, fill=id)) +
   guides(fill=FALSE) +labs(title="Evropa - osnovna slika")
 
-#Narišemo graf
+#Narišemo grafe
 
-graf<- ggplot(data= tabela_drzav,aes(x=drzave, y = seq(from = 0, to = 100, by = 10))) + 
-  geom_point(data=tabela_drzav,aes(x=drzave,y= dnevni_kadilci),color="red") + 
-  geom_point(data=tabela_drzav, aes(x = drzave, y= s_povisano_telesno_tezo),color="green")+
-  geom_point(data=tabela_drzav, aes(x = drzave, y= mesecno_prekomerno_pijancevanje),color="blue") +
-  geom_point(data=tabela_drzav, aes(x = drzave, y= niso_telesno_aktivni),color="orange") + 
-  geom_point(data=tabela_drzav, aes(x = drzave, y= nic_obrokov_sadja_in_zelenjave),color="purple") 
+#graf 6 držav- 3 z najvišjim BDP(Luxemburg,Norveška in Dansa) in 3 z najnižjim (Bolgarija, Romanija, Turčija)
+drzave_BDP <- c("Bulgaria","Romania","Turkey","Luxembourg"," Norway", "Denmark", "Sweden")
+graf_drzav_BDP <- ggplot(data= tabela_drzav %>% filter(drzave %in% drzave_BDP),aes(x=drzave, y = seq(from = 0, to = 100, by = 10))) + 
+  geom_point(data= tabela_drzav %>% filter(drzave %in% drzave_BDP),aes(x=drzave,y= dnevni_kadilci),color="red", show.legend = TRUE) + 
+  geom_point(data= tabela_drzav %>% filter(drzave %in% drzave_BDP), aes(x =drzave, y= s_povisano_telesno_tezo),color="green", show.legend = TRUE)+
+  geom_point(data= tabela_drzav %>% filter(drzave %in% drzave_BDP), aes(x =drzave, y= mesecno_prekomerno_pijancevanje),color="blue",show.legend = TRUE) +
+  geom_point(data= tabela_drzav %>% filter(drzave %in% drzave_BDP), aes(x =drzave, y= niso_telesno_aktivni),color="orange",show.legend = TRUE) + 
+  geom_point(data= tabela_drzav %>% filter(drzave %in% drzave_BDP), aes(x =drzave, y= nic_obrokov_sadja_in_zelenjave),color="purple",show.legend = TRUE) 
+print(graf_drzav_BDP)
+graf_drzav_BDP <- graf_drzav_BDP+ theme(legend.position = "right") +
+  scale_fill_continuous(guide = "colourbar") +
+  scale_size(guide = "legend")
 
-print(graf)
+# graf glede na stopnnjo izobrazbe
+graf_izobrazba <- ggplot(data= tabela_izobrazbe_spol %>% filter(spol == 'Total'),aes(x=izobrazba, y = seq(from = 0, to = 100, by = 10))) + 
+  geom_point(data=tabela_izobrazbe_spol %>% filter(spol == 'Total'),aes(x= izobrazba,y= dnevni_kadilci),color="red", show.legend = TRUE) + 
+  geom_point(data= tabela_izobrazbe_spol %>% filter(spol == 'Total'), aes(x =izobrazba, y= s_povisano_telesno_tezo),color="green", show.legend = TRUE)+
+  geom_point(data= tabela_izobrazbe_spol %>% filter(spol == 'Total'), aes(x =izobrazba, y= mesecno_prekomerno_pijancevanje),color="blue",show.legend = TRUE) +
+  geom_point(data= tabela_izobrazbe_spol %>% filter(spol == 'Total'), aes(x =izobrazba, y= niso_telesno_aktivni),color="orange",show.legend = TRUE) + 
+  geom_point(data= tabela_izobrazbe_spol %>% filter(spol == 'Total'), aes(x =izobrazba, y= nic_obrokov_sadja_in_zelenjave),color="purple",show.legend = TRUE) 
+graf_izobrazba <- graf_izobrazba+ theme(legend.position = "right") +
+  scale_fill_continuous(guide = "colourbar") +
+  scale_size(guide = "legend")
 
-najvisji.BDP <- c("Luxembourg"," Norway", "Denmark", "Sweden","Ireland")
-najnizji.BDP <- c("Bulgaria","Romania","Turkey","Croatia")
-graf_najvisjih <- ggplot(data= tabela_drzav %>% filter(drzave %in% najvisji.BDP),aes(x=drzave, y = seq(from = 0, to = 100, by = 10))) + 
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najvisji.BDP),aes(x=drzave,y= dnevni_kadilci),color="red") + 
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najvisji.BDP), aes(x =drzave, y= s_povisano_telesno_tezo),color="green")+
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najvisji.BDP), aes(x =drzave, y= mesecno_prekomerno_pijancevanje),color="blue") +
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najvisji.BDP), aes(x =drzave, y= niso_telesno_aktivni),color="orange") + 
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najvisji.BDP), aes(x =drzave, y= nic_obrokov_sadja_in_zelenjave),color="purple") 
+print(graf_izobrazba)
 
-graf_najnizji<- ggplot(data= tabela_drzav %>% filter(drzave %in% najnizji.BDP),aes(x=drzave, y = seq(from = 0, to = 100, by = 10))) + 
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najnizji.BDP),aes(x=drzave,y= dnevni_kadilci),color="red") + 
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najnizji.BDP), aes(x =drzave, y= s_povisano_telesno_tezo),color="green")+
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najnizji.BDP), aes(x =drzave, y= mesecno_prekomerno_pijancevanje),color="blue") +
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najnizji.BDP), aes(x =drzave, y= niso_telesno_aktivni),color="orange") + 
-  geom_point(data= tabela_drzav %>% filter(drzave %in% najnizji.BDP), aes(x =drzave, y= nic_obrokov_sadja_in_zelenjave),color="purple") 
-
-print(graf_najvisjih)
-print(graf_najnizji)
-#graf_kadilcev <- ggplot(tabela_drzav %>% filter(dnevni_kadilci > 20), aes(x=drzave, y=dnevni_kadilci,color= drzave)) + geom_point() + theme_bw()
-#print(graf_kadilcev)
-
+#graf glede na spol
+graf_spol <- ggplot(data= tabela_izobrazbe_spol %>% filter(izobrazba == 'All ISCED 2011 levels'),aes(x= spol, y = seq(from = 0, to = 100, by = 10))) + 
+  geom_point(data=tabela_izobrazbe_spol %>% filter(izobrazba == 'All ISCED 2011 levels'),aes(x= spol,y= dnevni_kadilci),color="red", show.legend = TRUE) + 
+  geom_point(data= tabela_izobrazbe_spol %>% filter(izobrazba == 'All ISCED 2011 levels'), aes(x =spol, y= s_povisano_telesno_tezo),color="green", show.legend = TRUE)+
+  geom_point(data= tabela_izobrazbe_spol %>% filter(izobrazba == 'All ISCED 2011 levels'), aes(x =spol, y= mesecno_prekomerno_pijancevanje),color="blue",show.legend = TRUE) +
+  geom_point(data= tabela_izobrazbe_spol %>% filter(izobrazba == 'All ISCED 2011 levels'), aes(x =spol, y= niso_telesno_aktivni),color="orange",show.legend = TRUE) + 
+  geom_point(data= tabela_izobrazbe_spol %>% filter(izobrazba == 'All ISCED 2011 levels'), aes(x =spol, y= nic_obrokov_sadja_in_zelenjave),color="purple",show.legend = TRUE) 
+print(graf_spol)
